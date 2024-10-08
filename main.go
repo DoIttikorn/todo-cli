4cdefed07d737d24c354d6b57749605f85eabe54
Ittikorn Chawkamud <aseaways1234@gmail.com>
2024-10-08 21:34:46 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type status int
+
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+func (t Task) FilterValue() string {
+	return t.title
+}
+
+func (t Task) Title() string {
+	return t.title
+}
+
+func (t Task) Description() string {
+	return t.description
+}
+
+type Model struct {
+	list list.Model
+	err  error
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+func (m *Model) InitList(width, height int) {
+	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+	m.list.Title = "To Do"
+	m.list.SetItems([]list.Item{
+		Task{status: todo, title: "Write documentation", description: "Write a lot of documentation"},
+		Task{status: todo, title: "Write code", description: "Write a lot of code"},
+		Task{status: todo, title: "Write tests", description: "Write a lot of tests"},
+	})
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.InitList(msg.Width, msg.Height)
+	}
+
+	newListModel, cmd := m.list.Update(msg)
+	m.list = newListModel
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
+}
+
+func (m Model) View() string {
+	return m.list.View()
+}
+
+func main() {
+	m := New()
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		os.Exit(1)
+	}
+}
